b76c3ae122be366cad3dd1314cf9925413d60a65
mandy <xiaomanshi@clubfactory.com>
2023-02-22 15:04:45 +0800

    feat: upodate meta

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,15 @@ import env from './cons/env';
 const { prefixCls } = styles;
 
 function App(): JSX.Element {
-
   return (
-      <div className={prefixCls}>
-        {/* SEO related settings */}
-        <Helmet defaultTitle="KiwiDrop" titleTemplate="%s | KiwiDrop">
-          <html lang="en" data-version={env.PACKAGE_VERSION} />
-          <meta name="keywords" content="kiwidrop,KiwiDrop,Kiwi Drop" />
-          <meta name="description" content="KiwiDrop Website" />
-        </Helmet>
-       
-          <AppRouter />
-      </div>
+    <div className={prefixCls}>
+      {/* SEO related settings */}
+      <Helmet defaultTitle="KiwiDrop" titleTemplate="%s | KiwiDrop">
+        <html lang="en" data-version={env.PACKAGE_VERSION} />
+      </Helmet>
+
+      <AppRouter />
+    </div>
   );
 }
 
